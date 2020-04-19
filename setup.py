db09ad5d4309bbc6b9ad38ad70ab4ba5ca67461d
mpariente <pariente.mnl@gmail.com>
2020-04-19 16:37:06 +0200

    Release v0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='torch_stoi',
-    version='0.0.1',
+    version='0.0.2',
     description='Computes Short Term Objective Intelligibility in PyTorch',
     author='Manuel Pariente',
     author_email='pariente.mnl@gmail.com',
--- torch_stoi/__init__.py
+++ torch_stoi/__init__.py
@@ -1,4 +1,4 @@
 from .stoi import NegSTOILoss
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
