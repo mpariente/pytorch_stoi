c376be1997750ec994cb5739df5647faf84c8eff
mpariente <pariente.mnl@gmail.com>
2020-04-21 13:46:36 +0200

    Add torchaudio to requirements.txt

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
-    install_requires=['numpy', 'torch', 'pystoi'],
+    install_requires=['numpy', 'torch', 'pystoi', 'torchaudio'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
